b69f9882654a65865a64fb9fe2fc074bb483c4c2
vhparikh <42686619+vhparikh@users.noreply.github.com>
2020-09-14 12:19:09 -0700

    Logic for Guessing Game

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -14,7 +14,20 @@ int main()
     int tries = 0;
     cout << "Try to guess the random number between 0  and 100" << endl;
     do {
-      
+      cin >> guess;
+      tries++;
+      if (guess < randomNum) {
+	cout << "Guess higher!" << endl;
+      }
+      else if (guess > randomNum) {
+	cout << "Guess lower!" << endl;
+      }
+      else {
+	cout << "Congratulations you guessed the right number!" << endl;
+	cout << "You took " << tries << " tries" << endl;
+	cout << "Would you like to play again? y/n" << endl;
+	cin >> isPlaying;
+      }
     } while (guess != randomNum);
   }
 }
